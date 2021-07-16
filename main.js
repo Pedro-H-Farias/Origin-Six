74eec35275b9b66b9eb0184a43ca753590273efc
Pedro-H-Farias <pedronutrinorte@gmail.com>
2021-07-16 15:25:16 -0400

    sexta

--- main.js
+++ main.js
@@ -1,1 +1,80 @@
-alert('eu existo')
+// abre e fecha menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//quando clicar no icone do menu esconder o menu
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// mudar o header da pagina ao scroll
+
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeith = header.offsetHeight
+
+  if (window.scrolly >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+//testimonial slider swiper
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// scrollreveal
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+//botao volta pro topo
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
